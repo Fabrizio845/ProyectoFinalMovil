03a794ed9aab060dea749423fef9e5ce863c035e
Fabrizio845 <fabrizio.calderon@tecsup.edu.pe>
2024-10-31 14:18:19 -0500

    Initial Commit

---
+++ ProyectoFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoFinal
+//
+//  Created by fabrizio  calderon on 31/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
